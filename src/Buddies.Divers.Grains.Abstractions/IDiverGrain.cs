7154702241f3ece743c98071b65d2cf2716404a4
Isaac Hili <18428880+isaachili@users.noreply.github.com>
2022-04-16 12:17:54 +0200

    WIP: Create diver grain

---
+++ src/Buddies.Divers.Grains.Abstractions/IDiverGrain.cs
@@ -0,0 +1,42 @@
+﻿using Builders.Divers.Abstractions.Entities;
+using Orleans;
+
+namespace Buddies.Divers.Grains.Abstractions
+{
+	/// <summary>
+	/// A grain that represents a diver that's responsible
+	/// for performing CRUD operations.
+	/// </summary>
+	public interface IDiverGrain : IGrainWithStringKey
+	{
+		/// <summary>
+		/// Creates an account for the diver if it doesn't exist.
+		/// </summary>
+		/// <param name="account">Diver account</param>
+		/// <remarks>
+		/// Throws an <see cref="InvalidOperationException"/>
+		/// when the diver is already associated with an account.
+		/// </remarks>
+		Task CreateAsync(Account account);
+
+		/// <summary>
+		/// Updates the diver's profile.
+		/// </summary>
+		/// <param name="profile">Diver profile</param>
+		/// <remarks>
+		/// Throws an <see cref="InvalidOperationException"/>
+		/// when the profile does not reflect that of an
+		/// existing diver.
+		/// </remarks>
+		Task UpdateProfileAsync(Profile profile);
+
+		/// <summary>
+		/// Deletes the diver's account from the system.
+		/// </summary>
+		/// <remarks>
+		/// Throws an <see cref="InvalidOperationException"/>
+		/// when the diver does not exist.
+		/// </remarks>
+		Task DeleteAsync();
+	}
+}
---
+++ src/Buddies.Divers.Grains/DiverGrain.cs
@@ -0,0 +1,83 @@
+﻿using Buddies.Divers.Grains.Abstractions;
+using Builders.Divers.Abstractions.Entities;
+using Orleans;
+
+namespace Buddies.Divers.Grains
+{
+	internal class DiverGrain : Grain, IDiverGrain
+	{
+		#region Properties
+
+		/// <summary>
+		/// Diver identifier
+		/// </summary>
+		public string DiverID { get; private set; } = null!;
+
+		/// <summary>
+		/// Diver account and profile
+		/// </summary>
+		public Account? Account { get; private set; }
+
+		#endregion
+
+		#region Grain Lifetime
+
+		public override async Task OnActivateAsync()
+		{
+			await base.OnActivateAsync();
+
+			// Retrieve identifier
+			DiverID = this.GetPrimaryKeyString();
+
+			// TODO - Get diver account and profile from database
+		}
+
+		#endregion
+
+		public async Task CreateAsync(Account account)
+		{
+			// Ensure that diver does not exist
+			if (Account is not null)
+			{
+				throw new InvalidOperationException($"Account '{DiverID}' already exists.");
+			}
+
+			// Update account record
+			Account = account;
+
+			// Persist changes
+			await CreateOrUpdateAsync();
+		}
+
+		public async Task UpdateProfileAsync(Profile profile)
+		{
+			// Ensure that diver exists
+			if (Account is null)
+			{
+				throw new InvalidOperationException($"Account '{DiverID}' does not exist.");
+			}
+
+			// Update profile record
+			Account.Profile = profile;
+
+			// Persist changes
+			await CreateOrUpdateAsync();
+		}
+
+		public Task DeleteAsync()
+		{
+			// Ensure that diver exists
+			if (Account is null)
+			{
+				throw new InvalidOperationException($"Account '{DiverID}' does not exist.");
+			}
+
+			throw new NotImplementedException();
+		}
+
+		private async Task CreateOrUpdateAsync()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
