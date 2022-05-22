da19c730d80dbf62a96d50c4006923f584bee734
Isaac Hili <18428880+isaachili@users.noreply.github.com>
2022-05-22 17:49:53 +0200

    Create service for data persistence of diver entities

---
+++ src/Buddies.Divers.Data.Abstractions/Services/IDiverService.cs
@@ -0,0 +1,20 @@
+﻿using Builders.Divers.Abstractions.Entities;
+
+namespace Buddies.Divers.Data.Abstractions.Services
+{
+	public interface IDiverService
+	{
+		/// <summary>
+		/// Persists changes to an <see cref="Account"/> entity.
+		/// </summary>
+		/// <param name="diverId">Diver ID</param>
+		/// <param name="account">Account</param>
+		Task CreateOrUpdateAsync(string diverId, Account account);
+
+		/// <summary>
+		/// Deletes a diver's account.
+		/// </summary>
+		/// <param name="diverId">Diver ID</param>
+		Task DeleteAsync(string diverId);
+	}
+}
